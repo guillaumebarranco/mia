a508872b4967e7fdbf7e986a9c67d1cb34e721cd
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-01-22 17:57:00 +0100

    update try read mp3

--- functions.php
+++ functions.php
@@ -203,6 +203,17 @@
 
 	var_dump($text);
 
+	$m = new SWFMovie();
+	$m->setRate(12.0);
+	$m->streamMp3(file_get_contents($text));
+	// use your own MP3
+
+	// The file is 11.85 seconds at 12.0 fps = 142 frames
+	$m->setFrames(142);
+
+	header('Content-type: application/x-shockwave-flash');
+	$m->output();
+
 	//exec('mpg321 '.urlencode($text));
 	
 ?>
