529ec5bb5d1fdaf1108a48c7ef3ede6a5dddbfcb
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-02-07 01:52:44 +0100

    test for gpio rasp

--- classes/MiaRealWorld.class.php
+++ classes/MiaRealWorld.class.php
@@ -4,6 +4,11 @@ class MiaRealWorld extends Mia {
 
 	public function brightLight() {
 
+		$koko = exec("cat /sys/class/leds/led1/brightness");
+
+		var_dump($koko);
+		die;
+
 		if("is off") {
 			exec('echo 1 => /devices/led1');
 			return $this->echoGoogle('La lumière est déjà allumée.');
