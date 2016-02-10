04f8cedfe431a1c3c56980a6fed42b6b8a990206
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-02-10 21:22:54 +0100

    [FEATURE] add more to real world class

--- classes/MiaRealWorld.class.php
+++ classes/MiaRealWorld.class.php
@@ -2,28 +2,30 @@
 
 class MiaRealWorld extends Mia {
 
+	private $led_path = "/sys/class/leds/led1";
+
 	public function turnOnLight() {
 
-		$koko = exec("cat /sys/class/leds/led1/brightness");
+		$koko = exec("cat ".$this->led_path."/brightness");
 
 		if($koko === "255") {
 			return $this->echoGoogle('La lumière est déjà allumée.');
 		}
 
-		$koko = exec("echo 255 > /sys/class/leds/led1/brightness");
+		$koko = exec("echo 255 > ".$this->led_path."/brightness");
 
 		return $this->echoGoogle('La lumière a été allumée');
 	}
 
 	public function turnOffLight() {
 
-		$koko = exec("cat /sys/class/leds/led1/brightness");
+		$koko = exec("cat ".$this->led_path."/brightness");
 
 		if($koko === "0") {
 			return $this->echoGoogle('La lumière est déjà éteinte.');
 		}
 
-		$koko = exec("echo 0 > /sys/class/leds/led1/brightness");
+		$koko = exec("echo 0 > ".$this->led_path."/brightness");
 
 		return $this->echoGoogle('La lumière a été éteinte');
 	}
