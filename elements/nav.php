4f334c1fad0f2a93639e2a9fa43db00deb73bf4e
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2016-11-07 00:34:12 +0100

    [FEATURE] Add todo screen func

--- elements/nav.php
+++ elements/nav.php
@@ -10,6 +10,7 @@
   <a href="index.php?template=tennis" class="menu-item"> <i class="fa fa-dot-circle-o"></i> </a>
   <a href="index.php?template=agendacassandre" class="menu-item"> <i class="fa fa-calendar"></i> </a>
   <a href="index.php?template=configuration" class="menu-item"> <i class="fa fa-cog"></i> </a>
+  <a href="index.php?template=todo" class="menu-item"> <i class="fa fa-list"></i> </a>
   <a href="index.php" class="menu-item"> <i class="fa fa-home"></i> </a>
   <a href="index.php" class="menu-item"> <i class="fa fa-home"></i> </a>
 
---
+++ elements/template/todo.template.php
@@ -0,0 +1,7 @@
+<ul>
+	<li>Ajouter plus d'éléments "boutons" comme pour les applications Android au clic sur le menu (ajouter bouton météo, bouton température, date....)</li>
+
+	<li>Ajouter du caractère à Mia. Pourcentage d'humour, pourcentage de sérieux etc...</li>
+	
+	<li>Ajouter un mode admin</li>
+</ul>
